fdec637f16cbf78d18481c2f0a7beda58e81a538
lichee <319licheng@163.com>
2014-08-20 15:32:09 +0800

    添加readme和license

---
+++ README.md
@@ -0,0 +1,12 @@
+# JBrick - 静态html/md生成器 #
+类似jekyll的简明的静态博客生成器，用来生成html。
+
+##1.打jar包##
+用eclipse或其他工具导出runnable jar包,入口类为net.nicecoder.jbrick.cli.Jbrick
+
+##2.使用指南
+生成文件：java -jar Jbrick.jar -b
+启动服务： java -jar Jbrick.jar -s -p 8080
+
+##3.建议
+有任何建议可以在开源中国社区上@无争，或者邮件nicecoder_net@126.com。
--- src/test/java/net/nicecoder/jbrick/AppTest.java
+++ src/test/java/net/nicecoder/jbrick/AppTest.java
@@ -1,12 +1,5 @@
 package net.nicecoder.jbrick;
 
-import java.io.IOException;
-
-import net.nicecoder.jbrick.model.App;
-import net.nicecoder.jbrick.server.BrickServer;
-
-import org.junit.Test;
-import org.pegdown.PegDownProcessor;
 
 public class AppTest {
 
@@ -24,13 +17,6 @@ public class AppTest {
 		}
 		System.out.println( "Listening on port "+8082+". Hit Enter to stop.\n" );
 		try { System.in.read(); } catch( Throwable t ) {};
-	}
-	
-	@Test
-	public void md(){
-		PegDownProcessor p = new PegDownProcessor();
-		String s = p.markdownToHtml("1. yyy\n2. sgsr\n");
-		System.out.println(s);
 	}*/
 	
 }
